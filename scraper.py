54910855a6bbb7432a4122dde5296775f9b642b2
parzivale <zeus@theolivers.org>
2020-11-25 23:20:40 +0100

    further imporved scraper removed unecesarry files

--- scraper.py
+++ scraper.py
@@ -3,55 +3,14 @@ import requests
 import sqlite3
 
 
-class product():
-    def __init__(self, name, price, image):
-        self.name = name
-        self.price = price
-        self.image = image
+def call_site(name):
+    return requests.get(name).content
 
 
-pccomponentes = requests.get("https://www.pccomponentes.com/procesadores/amd")
-
-
-
-soup = BeautifulSoup(pccomponentes.content, 'lxml')
+soup = BeautifulSoup(call_site("https://www.pccomponentes.com/procesadores/amd"), 'lxml')
 products = soup.find("div", id="articleListContent")
 
 
 
-for item in products.find_all("a"):
-    print(item.get_text())
-
-
-
-
-for item in products.find_all("a"):
-    cpu.append(item.get_text().replace(" ","-"))
-    
-
-for item in products.find_all("span"):
-    if item.get_text()[-1::] == "€":
-        price.append(item.get_text().replace(" ","-"))
-
-for image in products.find_all("img"):
-    images.append(str(image["src"]))
-
-conn = sqlite3.connect(r"C:\Users\zeus\OneDrive\Documents\pcpartpicker_alternative\products.db")
-
-c = conn.cursor()
-
-# Create table
-
-# Insert a row of data
-
-for item in range(len(images)):
-    c.execute("""INSERT INTO products(name, price, image) 
-               VALUES (?,?,?);""", (cpu[item], price[item], images[item]))
-
-# Save (commit) the changes
-conn.commit()
-
-# We can also close the connection if we are done with it.
-# Just be sure any changes have been committed or they will be lost.
-conn.close()
-
+for item in products.find_all("article"):
+    print(item.attrs["data-name"],item.attrs["data-price"],item.find("img").attrs["src"])
