35ec5d8c0046d162c0a19460208da4d0f6118b64
parzivale <zeus@theolivers.org>
2020-11-25 23:41:18 +0100

    finishing up the test data collection

--- scraper.py
+++ scraper.py
@@ -1,16 +1,21 @@
 from bs4 import BeautifulSoup
 import requests
 import sqlite3
-
+import sqlalchemy
 
 def call_site(name):
     return requests.get(name).content
 
 
-soup = BeautifulSoup(call_site("https://www.pccomponentes.com/procesadores/amd"), 'lxml')
+soup = BeautifulSoup(call_site("https://www.pccomponentes.com/procesadores"), 'lxml')
 products = soup.find("div", id="articleListContent")
 
 
+def create_list_pccom(array):
+    for item in products.find_all("article"):
+         array.append({"name": item.attrs["data-name"],"price": item.attrs["data-price"], "img": item.find("img").attrs["src"]})
 
-for item in products.find_all("article"):
-    print(item.attrs["data-name"],item.attrs["data-price"],item.find("img").attrs["src"])
+products_pccom = []
+create_list_pccom(products_pccom)
+
+print(str(products_pccom))
